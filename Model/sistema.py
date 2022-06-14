4259fd16e5782ed6cdec795908ac2930a9146ec0
Matheus Ed <matheus.lafeta.moc@gmail.com>
2022-06-14 02:48:21 -0300

    'Primeiras funcionalidades para o sistema integrando o controlador de contas ao menu e com opções para logar e alterar dados na conta'

--- Model/sistema.py
+++ Model/sistema.py
@@ -1,12 +1,15 @@
 from Controller.ctrlconta import *
 from View.telasistema import *
 
+
 class Sistema:
     def __init__(self):
-        self.controlador_conta = CtrlConta()
+        self.__controlador_conta = CtrlConta()
         self.__usuario_ativo = None
         self.__servidores = []
         self.__tela_sistema = TelaSistema()
+        self.diretorio = None
+        self.cota = None
 
     @property
     def usuario_ativo(self):
@@ -18,7 +21,7 @@ class Sistema:
 
     def logar(self):
         while True:
-            self.__usuario_ativo = self.controlador_conta.logar()
+            self.__usuario_ativo = self.__controlador_conta.logar()
             if self.usuario_ativo:
                 self.__tela_sistema.tela_login(1)
                 break
@@ -27,8 +30,25 @@ class Sistema:
                 break
 
     def menu_inical(self):
-        switcher = {1: self.logar, 2:self.controlador_conta.cadastrar_usuario()}
-        opcao = self.__tela_sistema.tela_menu_inicial()
-        if opcao == 0:
-            return
-        switcher[int(opcao)]()
+        switcher = {1: self.logar, 2: self.__controlador_conta.cadastrar_usuario}
+        while self.__usuario_ativo is None:
+            opcao = self.__tela_sistema.tela_menu_inicial()
+            if opcao == 0:
+                return
+            switcher[int(opcao)]()
+
+    def menu(self):
+        # implementar diretorio e cota
+        switcher = {1: self.diretorio, 2: self.__controlador_conta.ver_dados, 3: self.cota}
+        self.menu_inical()
+        while self.__usuario_ativo:
+            opcao = self.__tela_sistema.tela_menu()
+            if opcao == 0:
+                self.__usuario_ativo = None
+                self.menu_inical()
+            else:
+                switcher[opcao](self.usuario_ativo)
+
+
+oi = Sistema()
+oi.menu()
--- View/telasistema.py
+++ View/telasistema.py
@@ -24,3 +24,18 @@ class TelaSistema:
                 print('###Entrada Invalida!Digite um numero entre 0 e 2!###')  # ALTERAR NO FUTURO CONFORME ADICIONAR OPCAO
                 print('1 - Logar\n2 - Cadastrar Usuário\n0 - Fechar')
                 opcao = input('Opção: ')
+
+    def tela_menu(self):
+        print('1 - Diretorio \n2 - Ver dados de Usuario\n3 - Ver cota\n \n0 - Deslogar')
+        opcao = input('Opção: ')
+        while type(opcao) == str:
+            try:
+                opcao = int(opcao)
+                if 3 < opcao or opcao < 0:
+                    raise ValueError
+                return opcao
+            except ValueError:
+                print(
+                    '###Entrada Invalida!Digite um numero entre 0 e 3!###')  # ALTERAR NO FUTURO CONFORME ADICIONAR OPCAO
+                print('1 - Diretorio \n2 - Ver dados de Usuario\n3 - Ver cota\n \n0 - Deslogar')
+                opcao = input('Opção: ')
