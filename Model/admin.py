684c20c713f9c6ced6cfdb87aabf5713df37b545
João Vitor Duarte Domingos <jv.dd.senai.sc@gmail.com>
2022-06-19 14:31:26 -0300

    Excluindo pastas repitidas

--- Model/admin.py
+++
@@ -1,7 +0,0 @@
-from Model.conta import Conta
-
-
-class Admin(Conta):
-    def __init__(self, nome: str, cpf: int, email: str, senha: str, diretorio, empresa: str):
-        super().__init__(nome, cpf, email, senha, diretorio, empresa)
-        self.__admin = True
--- Model/usuario.py
+++
@@ -1,6 +0,0 @@
-from Model.conta import Conta
-
-class Usuario(Conta):
-    def __init__(self, nome: str, cpf: int, email: str, senha: str, diretorio, empresa: str):
-        super().__init__(nome, cpf, email, senha, diretorio, empresa)
-        self.__admin = False
