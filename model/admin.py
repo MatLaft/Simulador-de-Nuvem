09f78519d6865c9cadf855cea1d41ac21692d63b
Matheus Ed <matheus.lafeta.moc@gmail.com>
2022-06-20 16:13:54 -0300

    'Retirando atributo self.__admin'

--- model/admin.py
+++ model/admin.py
@@ -4,4 +4,3 @@ from model.conta import Conta
 class Admin(Conta):
     def __init__(self, nome: str, cpf: int, email: str, senha: str, diretorio, empresa: str):
         super().__init__(nome, cpf, email, senha, diretorio, empresa)
-        self.__admin = True
--- model/usuario.py
+++ model/usuario.py
@@ -4,4 +4,3 @@ from model.conta import Conta
 class Usuario(Conta):
     def __init__(self, nome: str, cpf: int, email: str, senha: str, diretorio: str, empresa: str):
         super().__init__(nome, cpf, email, senha, diretorio, empresa)
-        self.__admin = False
