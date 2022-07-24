d5339c488216396a29381e647a9e39acc66e17a2
Matheus Ed <matheus.lafeta.moc@gmail.com>
2022-07-24 17:16:11 -0300

    voltando erro do contaDAO

--- dao/diretorio_dao.py
+++ dao/diretorio_dao.py
@@ -1,4 +1,19 @@
+from dao.dao import DAO
+from model.diretorio import Diretorio
 
-class DiretorioDao():
+
+class DiretorioDao(DAO):
     def __init__(self):
-        pass
+        super().__init__('diretorio.pkl')
+
+    def add(self, diretorio: Diretorio):
+        super().add(diretorio.usuario.cpf, diretorio)
+
+    def remove(self, key):
+        return super().remove(key)
+
+    def get(self, key):
+        return super().get(key)
+
+    def get_all(self):
+        return super().get_all()
