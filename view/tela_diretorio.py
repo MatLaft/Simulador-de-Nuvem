296b4ced498f38252168faaccfc3ee7c0a53233c
João Vitor Duarte Domingos <jv.dd.senai.sc@gmail.com>
2022-07-24 19:12:41 -0300

    Correção bug não clicar em arquivo

--- view/tela_diretorio.py
+++ view/tela_diretorio.py
@@ -33,7 +33,6 @@ class TelaDiretorio:
             self.close()
             return 0
 
-
     def tela_ver_arquivos(self, lista_arquivos: []):
         lista_concatenada = []
         for index, arquivo in enumerate(lista_arquivos):
@@ -73,28 +72,30 @@ class TelaDiretorio:
             layout = [
                 [sg.Text('Não há arquivos para excluir', size=(30, 1),
                          font='Lucida', justification='left')],
-                 [sg.Button('Voltar', font=('Times New Roman', 12))]]
+                [sg.Button('Voltar', font=('Times New Roman', 12))]]
         else:
             lista_concatenada = []
             for index, arquivo in enumerate(lista_arquivos):
-                lista_concatenada.append(f'{index + 1} - {arquivo[0]} {arquivo[1]} {arquivo[2]} KB')
-            layout = [[sg.Text('Escolha o arquivo a ser excluído', size=(30, 1),
-                                 font='Lucida', justification='left')],
-                      [sg.Listbox(values=lista_concatenada,
-                                    select_mode='single', key='fac',
-                                    size=(100, 8), )],
-                      [sg.Button('Excluir', font=('Times New Roman', 12)),
-                       sg.Button('Voltar', font=('Times New Roman', 12))]]
+                lista_concatenada.append(
+                    f'{index + 1} - {arquivo[0]} {arquivo[1]} {arquivo[2]} KB')
+            layout = [
+                [sg.Text('Escolha o arquivo a ser excluído', size=(30, 1),
+                         font='Lucida', justification='left')],
+                [sg.Listbox(values=lista_concatenada,
+                            select_mode='single', key='fac',
+                            size=(100, 8), )],
+                [sg.Button('Excluir', font=('Times New Roman', 12)),
+                 sg.Button('Voltar', font=('Times New Roman', 12))]]
 
         self.__window = sg.Window('Excluir Arquivo').Layout(layout)
         event, value = self.__window.Read()
         if event == "Excluir":
             self.close()
-            return int(value['fac'][0][0]) - 1
+            if len(value['fac']) != 0:
+                return int(value['fac'][0][0]) - 1
         if event == "Voltar":
             self.close()
             return None
-
 
     def tela_cota(self, cota: int):
         self.mostra_mensagem(f"\nA cota restante é: {cota:.2f} KB")
